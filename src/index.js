7e6c83917e95651819b1dac276a5cbc07f57a932
ukshirsa <utkarshakshirsagar4717@gmail.com>
2023-03-20 15:45:03 +0530

    added loader on home screen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const express = require("express");
 // connecting to database
 const { connect } = require("./db/init");
 const app = express();
+app.get("/", (req, res) => {
+    res.send(
+      '<div style="width:200px; margin: auto auto;"><img width="100%"  src="https://media.tenor.com/2jd3xi2WVt0AAAAC/recurring-settings.gif"></div><div style="width:220px; margin: 0 auto;"><h2>Server is Running...</h2></div>'
+    );
+  });
 const PORT = process.env.PORT || 5001;
 connect()
   .then(() => {
