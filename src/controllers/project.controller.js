ac950f45217591838a6f275b5d99d0f39849eb18
prodeltaforce <siddhagiri55555@gmail.com>
2023-03-25 07:07:41 +0000

    Update project.controller.js and project.route.js

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,25 +1,97 @@
-const projectService = require("../services/project.service");
+// const projectService = require("../services/project.service");
+const Project = require("../models/project");
 const { Errors } = require("../constants");
 
-exports.getAllProjects = async (req, res) => {
+
+//Get all projects
+exports.getProjects = async (req, res, next) => {
   try {
-    const projects = await Project.find({});
-    res.send(projects);
+    const projects = await Project.find({
+      $or: [
+        { organizer: req.employee._id },
+        { contributors: req.employee._id },
+      ],
+    })
+    .populate("organizer", "_id name")
+    .populate("contributors", "_id name")
+    .populate("tasks");
+    res.json(projects);
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
+    next(error);
+    // console.error(error);
+    // res.status(500).send(error);
   }
 };
 
-exports.createNewProject = async (req, res) => {
+
+//Create a new Project
+exports.createNewProject = async (req, res, next) => {
   try {
-    const newProject = await projectService.createNewProject(req.body);
-    res.status(201).json(newEmployee);
-    // const project = new Project(req.body);
-    // await project.save();
-    // res.send(project);
+    const project = new Project({
+      name: req.body.name,
+      organizer: req.employee._id,
+      contributors: req.body.contributors || [],
+      tasks: [],
+    });
+    await project.save();
+    res.status(201).json(project);
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
+    // console.error(error);
+    // res.status(500).send(error);
+    next(error);
   }
 };
+
+
+//Get a Single project by _id
+exports.getProjectById = async(req, res, next) => {
+  try{
+    const project = await Project.findOne({ _id : req.params.id, 
+    $or : [{
+      { organizer: req.employee._id },
+      { contributors: req.employee._id },
+      }]
+    })
+    .populate("organizer", "_id name")
+    .populate("contributors", "_id name")
+    .populate("tasks");
+    if(!project){
+      return res.status(404).json({error : "Project Not Found"});
+    }
+    res.json(project);
+  } catch (error) {
+    next(error);
+  }
+}
+
+//Update a Project by ID
+exports.updateProjectById = async (req, res, next) => {
+  try {
+    const project = await Project.findOne({ _id: req.params.id, organizer: req.employee._id });
+    if (!project) {
+      return res.status(404).json({ error: 'Project not found' });
+    }
+    project.name = req.body.name;
+    project.contributors = req.body.contributors || [];
+    await project.save();
+    res.json(project);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+//Delete a Project by ID
+exports.deleteProjectById = async (req, res, next) => {
+  try {
+    const project = await Project.findOne({ _id: req.params.id, organizer: req.employee._id });
+    if (!project) {
+       return res.status(404).json({ error: 'Project not found' });
+    }
+    await project.remove();
+    res.json({ message: 'Project deleted successfully' });
+    } catch (error) {
+    next(error);
+  }
+};
+
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -2,5 +2,10 @@ const { Router } = require("express");
 const projectCtrl = require("../controllers/project.controller");
 const router = Router();
 
+router.get("/", projectCtrl.getProjects);
 router.post("/create", projectCtrl.createNewProject);
+router.get("/:id", projectCtrl.getProjectById);
+router.put("/:id", projectCtrl.updateProjectById);
+router.delete("/:id", projectCtrl / projectCtrl.deleteProjectById);
+
 module.exports = router;
